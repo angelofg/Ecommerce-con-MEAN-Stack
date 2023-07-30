07217e71a92f99a40b12cdbef3d6fc423dceabf5
angelofg <the.family186@gmail.com>
2023-07-30 10:10:18 -0400

    26.Eliminar un cupon

--- admin/src/app/components/cupones/index-cupon/index-cupon.component.ts
+++ admin/src/app/components/cupones/index-cupon/index-cupon.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CuponService } from 'src/app/services/cupon.service';
+declare var iziToast:any;
+declare var jQuery:any;
+declare var $:any;
 
 @Component({
   selector: 'app-index-cupon',
@@ -37,6 +40,37 @@ export class IndexCuponComponent implements OnInit {
         this.load_data = false;
       }
     )
+  }
+
+  eliminar(id:any){
+    this._cuponService.eliminar_cupon_admin(id,this.token).subscribe(
+      response=>{
+        iziToast.show({
+          title: 'SUCCESS',
+          titleColor: '#1DC74C',
+          color: '#FFF',
+          class: 'text-success',
+          position: 'topRight',
+          message: 'Se elimino correctamente el cliente.'
+        });
+
+        $('delete-'+id).modal('hide');
+        $('.modal-backdrop').removeClass('show');
+
+        this._cuponService.listar_cupones_admin(this.filtro,this.token).subscribe(
+          response=>{
+            this.cupones = response.data;
+            this.load_data = false;
+          }
+        )
+
+      },
+      error=>{
+        console.log(error);
+
+      }
+    )
+
   }
 
 }
