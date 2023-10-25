1ff666f460591f834624c13e3b2c5065730bba61
angelofg <the.family186@gmail.com>
2023-10-25 04:52:42 -0400

    62. Validar direccion principal de envio

--- back/controllers/ClienteController.js
+++ back/controllers/ClienteController.js
@@ -253,6 +253,15 @@ const actualizar_perfil_cliente_guest = async function(req,res){
 const registro_direccion_cliente = async function(req,res){
     if(req.user){
         var data = req.body; 
+
+        if(data.principal){
+            let direcciones = await Direccion.find({cliente: data.cliente});
+
+            direcciones.forEach(async element => {
+                await Direccion.findByIdAndUpdate({_id:element._id},{principal:false});
+            });
+        }
+
         let reg = await Direccion.create(data);
         res.status(200).send({data:reg}); 
     }else{
